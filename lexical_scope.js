49f4641e6424e4e834309896f92e14da36b86c8a
Muskan Valmikee <83203071+MuskanValmiki@users.noreply.github.com>
2021-11-09 22:06:26 +0530

    Add files via upload

---
+++ lexical_scope.js
@@ -0,0 +1,11 @@
+var a = "Hey Guys!..."
+function first(){
+    var b = "How are you  ";
+    second();
+    function second(){
+        var c = "Myself Megha Kashyap"
+        console.log(a,b,c)
+    }
+
+}
+first();
---
+++ variable.js
@@ -0,0 +1,10 @@
+const  data = "Muskan";
+console.log(data);
+
+var name = "muskan";
+var last = name + "Valmikee";
+console.log(last);
+
+let name_ = "Somi";
+name_ = "Samridhi";
+console.log(name_);
